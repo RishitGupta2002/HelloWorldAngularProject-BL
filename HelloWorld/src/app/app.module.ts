a990a95d1fa7b36e5ba84d8f316a312986a19154
Rishit Gupta <rishitgupta786gmail.com>
2025-03-22 16:18:00 +0530

    UC5 Code Added

--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -1,8 +1,10 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
